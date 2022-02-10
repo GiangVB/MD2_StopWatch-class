5c57b7088f16783ebca88802fb6fe7ac6bbab426
GiangVB <giangvb192@gmaiil.com>
2022-02-10 15:20:57 +0700

    Add project StopWatch-class

---
+++ README.md
@@ -0,0 +1,1 @@
+# Thực hành Java tạo lớp StopWatch #
---
+++ src/com/codegym/Main.java
@@ -0,0 +1,33 @@
+package com.codegym;
+
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] a = new int [100000];
+        for (int i = 0; i < 100000; i++) {
+            a[i] = (int)(Math.random()*1000);
+        }
+        LocalTime start = LocalTime.now();
+        a = selectionSort(a);
+        LocalTime end = LocalTime.now();
+        StopWatch stopWatch = new StopWatch(start,end);
+
+        System.out.println("Thời gian thực thi thuật toán sắp xếp 100,000 số: " + stopWatch.getElapsedTime() + " milli giây");
+    }
+
+    public static int[] selectionSort(int[] a){
+        for(int i = 0; i < a.length-1; i++){
+            for(int j = i+1; j < a.length; j++){
+                if(a[j] < a[i]){
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a;
+    }
+}
